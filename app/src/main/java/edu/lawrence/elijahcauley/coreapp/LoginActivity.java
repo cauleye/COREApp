5401cedb049d70456894430c511545758f8dfeb0
Malik Graham <malikgraham15@yahoo.com>
2015-11-13 15:02:45 -0600

    Added some code that implements a timer for the splash screen, and added the activity for that screen. Also, I added the URIHandler class.

--- app/src/main/java/edu/lawrence/elijahcauley/coreapp/LoginActivity.java
+++ app/src/main/java/edu/lawrence/elijahcauley/coreapp/LoginActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //the immersive login screen is created
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -17,6 +22,18 @@ public class LoginActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
         setContentView(R.layout.activity_login);
+        //this code sets a timer on the code in run
+        //the 0 represents a variable initial delay, before run starts
+        //the 5 represents a variable delay between each iteration of run
+        //and then we specify the unit we are using
+        final ScheduledExecutorService logInScreen = Executors.newSingleThreadScheduledExecutor();
+        logInScreen.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+
         //it's not consistent when we leave the application
 
     }
