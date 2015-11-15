bbc7954c1310eb7df33c8d814d0eca19a2259088
elijahcauley <elijah.cauley@lawrence.edu>
2015-11-14 21:11:09 -0600

    Added the DiscussionSelectActivity, DiscussionViewActivity, and OrientationHomeScreenActivity.

---
+++ app/src/main/java/edu/lawrence/elijahcauley/coreapp/ConnectionsDiscussionViewActivity.java
@@ -0,0 +1,13 @@
+package edu.lawrence.elijahcauley.coreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectionsDiscussionViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connections_discussion_view);
+    }
+}
