259f83bc73bf4652dfba60c96e26cbc42387b917
elijahcauley <elijah.cauley@lawrence.edu>
2015-11-15 18:48:40 -0600

    Added the home screen for Orientation, Resources, and Expectations.

--- app/src/main/java/edu/lawrence/elijahcauley/coreapp/ResourcesHomeScreenActivity.java
+++ app/src/main/java/edu/lawrence/elijahcauley/coreapp/ResourcesHomeScreenActivity.java
@@ -1,7 +1,8 @@
 package edu.lawrence.elijahcauley.coreapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class ResourcesHomeScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class ResourcesHomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources_home_screen);
+
+        String url = "https://moodle.lawrence.edu/course/view.php?id=6541";
+        WebView view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
     }
 }
