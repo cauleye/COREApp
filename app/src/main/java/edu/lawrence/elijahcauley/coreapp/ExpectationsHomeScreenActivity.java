2d7c6d43c6b52fd390f0bf6876cce2729b9177d1
elijahcauley <elijah.cauley@lawrence.edu>
2015-11-15 18:23:26 -0600

    Added the home screen for Orientation, Resources, and Expectations.

---
+++ app/src/main/java/edu/lawrence/elijahcauley/coreapp/ExpectationsHomeScreenActivity.java
@@ -0,0 +1,13 @@
+package edu.lawrence.elijahcauley.coreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpectationsHomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expectations_home_screen);
+    }
+}
